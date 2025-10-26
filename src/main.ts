a821419307a2e7fb14655c4f360aca8e50f788d3
Owolabi Idris <owolabiidris4@gmail.com>
2025-10-26 14:12:19 +0100

    updateed

--- src/main.ts
+++ src/main.ts
@@ -24,7 +24,7 @@ app.useGlobalPipes(
 
 
   app.use(json({ limit: '1mb' }));
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 8000;
   await app.listen(port);
   console.log(`Listening on ${port}`);
 }
